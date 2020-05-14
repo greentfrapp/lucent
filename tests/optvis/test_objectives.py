4073241ee0fdabad1ed74d5baa78125915b3a3a0
Lim Swee Kiat <limsweekiat@gmail.com>
2020-05-13 22:05:03 -0700

    Add pytest

---
+++ tests/optvis/test_objectives.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+
+import pytest
+
+import torch
+import numpy as np
+from lucent.optvis import objectives, param, render, transform
+from lucent.modelzoo import InceptionV1
+
+
+NUM_STEPS = 3
+
+
+@pytest.fixture
+def inceptionv1():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = InceptionV1().to(device)
+    model.eval()
+    return model
+
+
+def assert_gradient_descent(objective, model):
+    params, image = param.image(224)
+    optimizer = torch.optim.Adam(params, lr=0.1)
+    T = render.hook_model(model, image)
+    objective_f = objectives.as_objective(objective)
+    model(image())
+    start_value = objective_f(T)
+    for _ in range(NUM_STEPS):
+        optimizer.zero_grad()
+        model(image())
+        loss = objective_f(T)
+        loss.backward()
+        optimizer.step()
+    end_value = objective_f(T)
+    assert start_value > end_value
+
+
+def test_neuron(inceptionv1):
+    objective = objectives.neuron("mixed3a_1x1_pre_relu_conv", 0)
+    assert_gradient_descent(objective, inceptionv1)
+
+
+def test_channel(inceptionv1):
+    objective = objectives.channel("mixed3a_1x1_pre_relu_conv", 0)
+    assert_gradient_descent(objective, inceptionv1)
