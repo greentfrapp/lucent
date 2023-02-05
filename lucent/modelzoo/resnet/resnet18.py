cd0ca2e70bbc6b45e4eee0c9ee08ee50bdee6c39
atagade <atagade19@gmail.com>
2023-02-05 20:48:40 +1100

    Added options for skipping batchnorm and using linear modules

--- lucent/modelzoo/resnet/resnet18.py
+++ lucent/modelzoo/resnet/resnet18.py
@@ -4,9 +4,9 @@ from .resnet import build_resnet
 from .resnet import BasicBlock
 from torchvision.models.resnet import ResNet18_Weights
 
-def resnet18():
+def resnet18(use_linear_modules=False, skip_batchnorm=True):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     weights = ResNet18_Weights.verify("IMAGENET1K_V1")
-    model = build_resnet(BasicBlock, [2, 2, 2, 2], weights=weights, progress=True, use_linear_modules_only=False, skip_batchnorm = True)
+    model = build_resnet(BasicBlock, [2, 2, 2, 2], weights=weights, progress=True, use_linear_modules_only=use_linear_modules, skip_batchnorm = skip_batchnorm)
 
     return model
