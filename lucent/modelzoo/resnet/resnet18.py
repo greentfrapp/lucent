0817f3ad662d071aed9a4282ebdeec3b1ad31ca1
atagade <atagade19@gmail.com>
2023-02-05 20:41:19 +1100

    Fixed model device and eval

--- lucent/modelzoo/resnet/resnet18.py
+++ lucent/modelzoo/resnet/resnet18.py
@@ -7,6 +7,6 @@ from torchvision.models.resnet import ResNet18_Weights
 def resnet18():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     weights = ResNet18_Weights.verify("IMAGENET1K_V1")
-    model = build_resnet(BasicBlock, [2, 2, 2, 2], weights=weights, progress=True, use_linear_modules_only=False, skip_batchnorm = True).to(device).eval()
+    model = build_resnet(BasicBlock, [2, 2, 2, 2], weights=weights, progress=True, use_linear_modules_only=False, skip_batchnorm = True)
 
     return model
