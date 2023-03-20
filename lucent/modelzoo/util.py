92b366ac8c4ef3c256e2c3dd9fcc52edeb746bad
Roland Zimmermann <5895436+zimmerrol@users.noreply.github.com>
2023-03-20 15:19:46 +0100

    Add utility function to get layers based on their names (#5)

--- lucent/modelzoo/util.py
+++ lucent/modelzoo/util.py
@@ -18,7 +18,7 @@
 from __future__ import absolute_import, division, print_function
 
 from collections import OrderedDict
-from typing import List, Union
+from typing import List, Optional, Union
 
 import torch
 from torch import nn
@@ -28,8 +28,12 @@ def get_model_layers(
     model: nn.Module, get_layer_representation: bool = False
 ) -> Union[OrderedDict[str, str], List[str]]:
     """
-    If get_layer_representation is True, return a OrderedDict of layer names, layer representation
-    string pair. If it's False, just return a list of layer names
+    Args:
+        model: Model to get layer names for.
+        get_layer_representation: Get layer representations and not just layer names.
+    Returns:
+        If get_layer_representation is True, return a OrderedDict of layer names, layer representation
+        string pair. If it's False, just return a list of layer names
     """
     layer_name_representations = OrderedDict()
 
@@ -53,3 +57,35 @@ def get_model_layers(
         return layer_name_representations
     else:
         return list(layer_name_representations.keys())
+
+
+def get_model_layer(model: nn.Module, layer_name: str) -> Optional[nn.Module]:
+    """Get a model's layer based on its internal name.
+
+    Args:
+        model: Module to extract layer from.
+        layer_name: Name of the layer to extract.
+
+    Returns:
+        Extracted layer if the name is valid, None otherwise.
+    """
+
+    def get_layer(net, prefix=[]):
+        # Recursive function to test layers.
+        if hasattr(net, "_modules"):
+            for name, layer in net._modules.items():
+                if layer is None:
+                    # e.g. GoogLeNet's aux1 and aux2 layers
+                    continue
+                current_layer_name = "_".join(prefix + [name])
+                if current_layer_name == layer_name:
+                    return layer
+                inner = get_layer(layer, prefix=prefix + [name])
+                if inner:
+                    return inner
+        return None
+
+    if isinstance(model, torch.nn.DataParallel):
+        model = model.module
+
+    return get_layer(model)
